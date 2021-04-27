cc8a05526beac9831cd6f03698a2729236915b7e
Francesco Masala <francesco.masala@buonarroti.tn.it>
2021-04-27 10:49:42 +0200

    Updated tris.java

--- src/it/tn/buonarroti/masala/tris/Tris.java
+++ src/it/tn/buonarroti/masala/tris/Tris.java
@@ -1,23 +1,40 @@
 package it.tn.buonarroti.masala.tris;
 
+import java.util.Objects;
+
 public class Tris {
     private boolean[][] gioco;
     private int nMossa;
 
-    public Tris(){
+    public Tris() {
 
     }
 
-    public boolean mossaG(int col, int row){
+    public Boolean mossaG(int row, int col) {
+        Boolean result = Boolean.FALSE;
+        if (this.giocoValido() == Boolean.TRUE) {
+            if ((row >= 0 && row < 3) && (col >= 0 && col < 3)) {
+                if (Objects.equals(this.gioco[row][col], null)) {
+                    this.gioco[row][col] = Boolean.TRUE;
+                    result = Boolean.TRUE;
+                }
+            }
+        }
+        if ((Objects.equals(this.giocoValido(), Boolean.TRUE) && (Objects.equals(result, Boolean.TRUE)))){
+            this.mossaC();
+        }
         return true;
     }
-    private void mossaC(){
+
+    private void mossaC() {
 
     }
-    public boolean giocoValido(){
+
+    public boolean giocoValido() {
         return true;
     }
-    public Integer vincitore(){
+
+    public Integer vincitore() {
 
         return 0;
     }
