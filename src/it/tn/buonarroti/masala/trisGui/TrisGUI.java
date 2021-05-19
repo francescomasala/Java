c484a7f0b142e3e871b6af0c56ecb7f866ca522a
Francesco Masala <francesco.masala@buonarroti.tn.it>
2021-05-19 12:39:30 +0200

    Added logic and thread sleeping

--- src/it/tn/buonarroti/masala/trisGui/TrisGUI.java
+++ src/it/tn/buonarroti/masala/trisGui/TrisGUI.java
@@ -1,7 +1,8 @@
 package it.tn.buonarroti.masala.trisGui;
 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TrisGUI extends JDialog {
     private static int mossa = 0;
@@ -38,157 +39,183 @@ public class TrisGUI extends JDialog {
         row3col2.setEnabled(false);
         row3col3.setEnabled(false);
 
-        mainbutton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                giocatoreUno = JOptionPane.showInputDialog("Nome del primo giocatore: ");
-                giocatoreDue = JOptionPane.showInputDialog("Nome del secondo giocatore: ");
+        mainbutton.addActionListener(e -> {
+            giocatoreUno = JOptionPane.showInputDialog("Nome del primo giocatore: ");
+            giocatoreDue = JOptionPane.showInputDialog("Nome del secondo giocatore: ");
 
-                mainbutton.setVisible(false);
+            mainbutton.setVisible(false);
 
-                row1col1.setEnabled(true);
-                row1col2.setEnabled(true);
-                row1col3.setEnabled(true);
-                row2col1.setEnabled(true);
-                row2col2.setEnabled(true);
-                row2col3.setEnabled(true);
-                row3col1.setEnabled(true);
-                row3col2.setEnabled(true);
-                row3col3.setEnabled(true);
+            row1col1.setEnabled(true);
+            row1col2.setEnabled(true);
+            row1col3.setEnabled(true);
+            row2col1.setEnabled(true);
+            row2col2.setEnabled(true);
+            row2col3.setEnabled(true);
+            row3col1.setEnabled(true);
+            row3col2.setEnabled(true);
+            row3col3.setEnabled(true);
+        });
+
+        row1col1.addActionListener(e -> {
+            if (!turno) {
+                row1col1.setText("X");
+            } else {
+                row1col1.setText("O");
+            }
+
+            turno = !turno;
+            row1col1.setEnabled(false);
+
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+
+        });
+
+        row1col2.addActionListener(e -> {
+            if (!turno) {
+                row1col2.setText("X");
+            } else {
+                row1col2.setText("O");
+            }
+
+            turno = !turno;
+            row1col2.setEnabled(false);
+
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row1col1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row1col1.setText("X");
-                } else {
-                    row1col1.setText("O");
-                }
+        row1col3.addActionListener(e -> {
+            if (!turno) {
+                row1col3.setText("X");
+            } else {
+                row1col3.setText("O");
+            }
 
-                turno = !turno;
-                row1col1.setEnabled(false);
+            turno = !turno;
+            row1col3.setEnabled(false);
 
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row1col2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row1col2.setText("X");
-                } else {
-                    row1col2.setText("O");
-                }
+        row2col1.addActionListener(e -> {
+            if (!turno) {
+                row2col1.setText("X");
+            } else {
+                row2col1.setText("O");
+            }
 
-                turno = !turno;
-                row1col2.setEnabled(false);
+            turno = !turno;
+            row2col1.setEnabled(false);
 
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row1col3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row1col3.setText("X");
-                } else {
-                    row1col3.setText("O");
-                }
+        row2col2.addActionListener(e -> {
+            if (!turno) {
+                row2col2.setText("X");
+            } else {
+                row2col2.setText("O");
+            }
 
-                turno = !turno;
-                row1col3.setEnabled(false);
+            turno = !turno;
+            row2col2.setEnabled(false);
 
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row2col1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row2col1.setText("X");
-                } else {
-                    row2col1.setText("O");
-                }
+        row2col3.addActionListener(e -> {
+            if (!turno) {
+                row2col3.setText("X");
+            } else {
+                row2col3.setText("O");
+            }
 
-                turno = !turno;
-                row2col1.setEnabled(false);
+            turno = !turno;
+            row2col3.setEnabled(false);
 
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row2col2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row2col2.setText("X");
-                } else {
-                    row2col2.setText("O");
-                }
+        row3col1.addActionListener(e -> {
+            if (!turno) {
+                row3col1.setText("X");
+            } else {
+                row3col1.setText("O");
+            }
 
-                turno = !turno;
-                row2col2.setEnabled(false);
+            turno = !turno;
+            row3col1.setEnabled(false);
 
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row2col3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row2col3.setText("X");
-                } else {
-                    row2col3.setText("O");
-                }
+        row3col2.addActionListener(e -> {
+            if (!turno) {
+                row3col2.setText("X");
+            } else {
+                row3col2.setText("O");
+            }
 
-                turno = !turno;
-                row2col3.setEnabled(false);
+            turno = !turno;
+            row3col2.setEnabled(false);
 
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        row3col1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row3col1.setText("X");
-                } else {
-                    row3col1.setText("O");
-                }
-
-                turno = !turno;
-                row3col1.setEnabled(false);
-
-                TrisGUI.mossa++;
+        row3col3.addActionListener(e -> {
+            if (!turno) {
+                row3col3.setText("X");
+            } else {
+                row3col3.setText("O");
             }
-        });
 
-        row3col2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row3col2.setText("X");
-                } else {
-                    row3col2.setText("O");
-                }
+            turno = !turno;
+            row3col3.setEnabled(false);
 
-                turno = !turno;
-                row3col2.setEnabled(false);
-
-                TrisGUI.mossa++;
-            }
-        });
-
-        row3col3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (turno == false) {
-                    row3col3.setText("X");
-                } else {
-                    row3col3.setText("O");
-                }
-
-                turno = !turno;
-                row3col3.setEnabled(false);
-
-                TrisGUI.mossa++;
+            TrisGUI.mossa++;
+            try {
+                annunciaVincitore();
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
@@ -199,15 +226,7 @@ public class TrisGUI extends JDialog {
                 onCancel();
             }
         });
-
-        // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
-
 
 
     public static void main(String[] args) {
@@ -217,17 +236,70 @@ public class TrisGUI extends JDialog {
         System.exit(0);
     }
 
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
-
     private void onCancel() {
-        // add your code here if necessary
         dispose();
     }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public void annunciaVincitore() throws InterruptedException {
+        // Caso di default se nessuna situazione accade
+        Character vincitore;
+
+        //Crea array
+        Character[][] valoriTris = new Character[3][3];
+
+        if (!row1col1.getText().equals("Cliccami!")) valoriTris[0][0] = ((row1col1.getText().equals("X")) ? '1' : '2');
+        if (!row1col2.getText().equals("Cliccami!")) valoriTris[0][1] = ((row1col2.getText().equals("X")) ? '1' : '2');
+        if (!row1col3.getText().equals("Cliccami!")) valoriTris[0][2] = ((row1col3.getText().equals("X")) ? '1' : '2');
+        if (!row2col1.getText().equals("Cliccami!")) valoriTris[1][0] = ((row2col1.getText().equals("X")) ? '1' : '2');
+        if (!row2col2.getText().equals("Cliccami!")) valoriTris[1][1] = ((row2col2.getText().equals("X")) ? '1' : '2');
+        if (!row2col3.getText().equals("Cliccami!")) valoriTris[1][2] = ((row2col3.getText().equals("X")) ? '1' : '2');
+        if (!row3col1.getText().equals("Cliccami!")) valoriTris[2][0] = ((row3col1.getText().equals("X")) ? '1' : '2');
+        if (!row3col2.getText().equals("Cliccami!")) valoriTris[2][1] = ((row3col2.getText().equals("X")) ? '1' : '2');
+        if (!row3col3.getText().equals("Cliccami!")) valoriTris[2][2] = ((row3col3.getText().equals("X")) ? '1' : '2');
+
+        vincitore = 'd';
+        if (mossa == 9) vincitore = 'p';
+        for (int i = 0; i < 3; i++) {
+            // Tris verticale
+            if (valoriTris[0][i] == valoriTris[1][i] && valoriTris[1][i] == valoriTris[2][i] && valoriTris[0][i] != null) {
+                vincitore = valoriTris[0][i];
+                // Tris orizzontale
+            } else if (valoriTris[i][0] == valoriTris[i][1] && valoriTris[i][1] == valoriTris[i][2] && valoriTris[i][0] != null) {
+                vincitore = valoriTris[i][0];
+            }
+        }
+        // Tris diagonale
+        if (valoriTris[1][1] != ' ') {
+            if (valoriTris[0][0] == valoriTris[1][1] && valoriTris[1][1] == valoriTris[2][2]) {
+                vincitore = valoriTris[1][1];
+            } else if (valoriTris[0][2] == valoriTris[1][1] && valoriTris[1][1] == valoriTris[2][0]) {
+                vincitore = valoriTris[1][1];
+            }
+        }
+
+        switch (vincitore) {
+
+            case '1':
+                JOptionPane.showMessageDialog(null, giocatoreUno + " ha vinto il gioco!");
+                dispose();
+                Thread.sleep(10000);
+                break;
+            case '2':
+                JOptionPane.showMessageDialog(null, giocatoreDue + " ha vinto il gioco!");
+                dispose();
+                Thread.sleep(10000);
+                break;
+            case 'p':
+                JOptionPane.showMessageDialog(null, "Pareggio!");
+                dispose();
+                Thread.sleep(10000);
+                break;
+            case 'd':
+                break;
+        }
     }
 }
