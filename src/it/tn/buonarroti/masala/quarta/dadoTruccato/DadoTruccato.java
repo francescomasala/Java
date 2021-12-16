791629fb3c6bd033708025768ae351fa65d02fdc
Francesco Masala <mail@francescomasala.me>
2021-12-16 10:10:17 +0100

    Updated Class DadoTruccato

---
+++ src/it/tn/buonarroti/masala/quarta/dadoTruccato/DadoTruccato.java
@@ -0,0 +1,21 @@
+package it.tn.buonarroti.masala.quarta.dadoTruccato;
+
+import java.util.Random;
+
+public class DadoTruccato extends Random {
+
+	public DadoTruccato(){
+		super();
+	}
+
+	@Override
+	public int nextInt(int num){
+		int ran = super.nextInt(2);
+		if (ran == 0){
+			return num-1;
+		} else {
+			int i = super.nextInt(num - 1);
+			return i;
+		}
+	}
+}
---
+++ src/it/tn/buonarroti/masala/quarta/dadoTruccato/MainDadoTruccato.java
@@ -0,0 +1,51 @@
+package it.tn.buonarroti.masala.quarta.dadoTruccato;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class MainDadoTruccato {
+	private String CheckDadoTruccato(int facceDado, int tiriPerControllo){
+		int iTrucc=0, iNorm=0, i=0, checkTruccato=0;
+		int[] datiTrucc = new int[tiriPerControllo+1];
+		int[] datiNorm = new int[tiriPerControllo+1];
+		DadoTruccato dado = new DadoTruccato();
+		Random random = new Random();
+
+		while (iTrucc < tiriPerControllo){
+			datiTrucc[iTrucc] = dado.nextInt(facceDado);
+			iTrucc++;
+		}
+
+		while (iNorm < tiriPerControllo){
+			datiNorm[iNorm] = random.nextInt(facceDado);
+			iNorm++;
+		}
+
+		iTrucc = 0;
+		iNorm = 0;
+
+		while (i < tiriPerControllo){
+			if (datiTrucc[iTrucc] == (facceDado-1)){
+				checkTruccato++;
+			}
+
+			if (datiNorm[iNorm] == (facceDado-1)){
+				checkTruccato--;
+			}
+			iTrucc++;
+			iNorm++;
+			i++;
+		}
+		return "La possibilità che il dado sia truccato è dell': " + checkTruccato + "0%";
+	}
+
+	public static void main(String[] args) {
+		DadoTruccato dado = new DadoTruccato();
+		MainDadoTruccato mainDado = new MainDadoTruccato();
+		System.out.println(dado.nextInt(7));
+		System.out.println(mainDado.CheckDadoTruccato(9,250000));
+	}
+
+
+}
