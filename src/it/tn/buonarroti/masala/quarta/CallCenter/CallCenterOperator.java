c5563c18e4ed25a58710cdf4b9d858a461e5f35d
Francesco Masala <mail@francescomasala.me>
2022-04-07 09:23:45 +0200

    Added CallCenter Class

---
+++ src/it/tn/buonarroti/masala/quarta/CallCenter/CallCenterOperator.java
@@ -0,0 +1,102 @@
+package it.tn.buonarroti.masala.quarta.CallCenter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CallCenterOperator {
+	public String FamilyName;
+	public String PersonName;
+	private Integer CallCenterID;
+	private String email;
+	private Boolean isOperatorFree;
+
+	public CallCenterOperator() {
+	}
+
+	public CallCenterOperator(String FamilyName, String PersonName,String email, Integer CallCenterID)
+			throws Exception {
+		this();
+		this.setFamilyName(FamilyName);
+		this.setPersonName(PersonName);
+		this.setCallCenterID(CallCenterID);
+		this.setEmail(email);
+	}
+
+	public void setOperatorFree(Boolean operatorFree) {
+		isOperatorFree = operatorFree;
+	}
+
+	public Boolean getOperatorFree() {
+		return isOperatorFree;
+	}
+
+	//nuovo in persona 6
+	public void setEmail(String email) throws Exception {
+		//Dichiarazione pattern e matcher
+		Pattern pattern = Pattern.compile("[a-z0-9._-]+@[a-z0-9._-]+.[a-z]");
+		Matcher matcher = pattern.matcher(email);
+
+		if(matcher.matches()){
+			this.email = email;
+		}else{
+			throw new Exception("L'email non rispetta il formato richiesto");
+		}
+	}
+
+	//Nuovo in persona 7
+	public void setCallCenterID(Integer callCenterID) throws Exception {
+			this.CallCenterID = callCenterID;	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Integer getCallCenterID() {
+		return CallCenterID;
+	}
+
+	public String getFamilyName() {
+		return FamilyName;
+	}
+
+	public void setFamilyName(String familyName) throws Exception {
+
+		if (familyName == null & familyName.length() < 1) throw new Exception("Surname has empty or null value");
+		else{
+			Pattern pattern = Pattern.compile("[a-zA-Zèòàù]+");
+			Matcher matcher = pattern.matcher(familyName);
+
+			if(matcher.matches()){
+				this.FamilyName = familyName;
+			}else{
+				throw new Exception("This surname does not respect regex parameters: [a-zA-Zèòàù]");
+			}
+		}
+	}
+
+	public String getPersonName() {
+		return PersonName;
+	}
+
+	public void setPersonName(String personName) throws Exception{
+		if (FamilyName == null & FamilyName.length() < 1) throw new Exception("Name has empty or null value");
+		else{
+			Pattern pattern = Pattern.compile("[a-zA-Zèòàù]+");
+			Matcher matcher = pattern.matcher(personName);
+
+			if(matcher.matches()){
+				this.PersonName = personName;
+			}else{
+				throw new Exception("This surname does not respect regex parameters: [a-zA-Zèòàù]");
+			}
+		}
+	}
+
+	//Aggiornato per persona 6
+	public String info() {
+		return "== Informazioni su: " + PersonName + " " + FamilyName + " =="
+				+ "\nEmail: " + email
+				+ "\nCall Center ID: " + CallCenterID;
+	}
+}
---
+++ src/it/tn/buonarroti/masala/quarta/CallCenter/PhoneCallCenter.java
@@ -0,0 +1,8 @@
+package it.tn.buonarroti.masala.quarta.CallCenter;
+
+import it.tn.buonarroti.masala.quarta.stack.linkedList.LinkedList;
+
+public class PhoneCallCenter<T> {
+
+
+}
