a77c036f36d7ff2410ac6fe64c2091c0f7c98a6e
Francesco Masala <mail@francescomasala.me>
2022-05-02 12:01:50 +0200

    Updated Semaphores

---
+++ src/it/tn/buonarroti/masala/tipsit/semafori/Main.java
@@ -0,0 +1,15 @@
+package it.tn.buonarroti.masala.tipsit.semafori;
+
+public class Main {
+	public static void main(String[] args) {
+		Cesto cestino = new Cesto();
+
+		Thread mensa1 = new Thread(new Mensa(cestino));
+		Thread mensa2 = new Thread(new Mensa(cestino));
+		Thread mensa3 = new Thread(new Mensa(cestino));
+
+		mensa1.run();
+		mensa2.run();
+		mensa3.run();
+	}
+}
