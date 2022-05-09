f3b9c29801308345c4c6129b485fd6e376d4b910
Francesco Masala <mail@francescomasala.me>
2022-05-09 12:01:39 +0200

    Fatto qualcosa con i runnable, non so tbh

---
+++ src/it/tn/buonarroti/masala/tipsit/contatoreSync/counter.java
@@ -0,0 +1,13 @@
+package it.tn.buonarroti.masala.tipsit.contatoreSync;
+
+public class counter {
+	public Integer counter=150;
+
+	public Integer getCounter() {
+		return counter;
+	}
+
+	public void addCounter(int i) {
+		this.counter = this.counter + i;
+	}
+}
