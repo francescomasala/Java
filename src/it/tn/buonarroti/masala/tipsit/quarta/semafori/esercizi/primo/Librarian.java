f2322eaa07f4c2fb95eff058daf5dc73d0e74f6c
Francesco Masala <mail@francescomasala.me>
2022-10-10 12:27:12 +0200

    Updated class

--- src/it/tn/buonarroti/masala/tipsit/quarta/semafori/esercizi/primo/Librarian.java
+++ src/it/tn/buonarroti/masala/tipsit/quarta/semafori/esercizi/primo/Librarian.java
@@ -15,26 +15,29 @@ public class Librarian implements Runnable {
 	@Override
 	public void run() {
 		while (true) {
-			if (BookBorrow.getQueueLength() >= 0) {
+			// if (BookBorrow.getQueueLength() >= 0) {
 				try {
-					System.out.println("[LIB] - I'm working!");
+					System.out.println("[LIB - " + Thread.currentThread().getName() + "] - I'm working!");
+					System.out.println("[LIB - " + Thread.currentThread().getName() + "] - There are: " + BookBorrow.getQueueLength() + " students waiting");
 					BookSearch.acquire();
-					System.out.println("[LIB] - I'm searching a book!");
 					Thread.sleep(2000);
-					System.out.println("[LIB] - I gave a book!");
+					if (BookSearch.getQueueLength() != 0) {
+						System.out.println("[LIB - " + Thread.currentThread().getName() + "] - I'm searching a book!");
+						System.out.println("[LIB - " + Thread.currentThread().getName() + "] - I gave a book!");
+						BookBorrow.release();
+					}
 					BookSearch.release();
-					BookBorrow.release();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			} else {
-				try {
-					System.out.println("[LIB] - I'm free!");
-					Thread.sleep(2000);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
+			// } else {
+			//	try {
+			//		System.out.println("[LIB] - I'm free!");
+			//		Thread.sleep(2000);
+			// 	} catch (Exception e) {
+			// 		e.printStackTrace();
+			// 	}
+			// }
 		}
 	}
 }
--- src/it/tn/buonarroti/masala/tipsit/quarta/semafori/esercizi/primo/Students.java
+++ src/it/tn/buonarroti/masala/tipsit/quarta/semafori/esercizi/primo/Students.java
@@ -13,30 +13,36 @@ public class Students implements Runnable{
 		this.BookBorrow = BookBorrow;
 		this.BookSearch = BookSearch;
 		this.BooksToRead = BooksToRead;
-
 	}
 
 
 	@Override
 	public void run() {
-		while (BooksToRead > 0) {
-			if (BookSearch.getQueueLength() == 0) {
-				try {
-					System.out.println("[STU] - I'm asking for a book!");
-					BookBorrow.acquire();
-					Thread.sleep(2000);
-					System.out.println("[STU] - I'm waiting!");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			} else if (BookSearch.getQueueLength() > 0){
-				try {
-					System.out.println("[STU] - The queue is full!");
-					Thread.sleep(2000);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		System.out.println("[STU - " + Thread.currentThread().getName() + "] - Need to read " + BooksToRead + " books");
+		while (BooksRead <= BooksToRead) {
+			//if (BookSearch.getQueueLength() == 0) {
+			try {
+				System.out.println("[STU - " + Thread.currentThread().getName() + "] - I'm asking for a book!");
+				BookBorrow.acquire();
+				System.out.println("[STU - " + Thread.currentThread().getName() + "] - I'm waiting!");
+				BookSearch.acquire();
+				Thread.sleep(2000);
+				System.out.println("[STU - " + Thread.currentThread().getName() + "] - I took the book!");
+				BookBorrow.release();
+				BookSearch.release();
+				System.out.println("[STU - " + Thread.currentThread().getName() + "] - I've read " + BooksRead + " books of" + BooksToRead);
+				BooksRead++;
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
+			//} else if (BookSearch.getQueueLength() > 0){
+			//	try {
+			//		System.out.println("[STU] - The queue is full!");
+			//		Thread.sleep(2000);
+			//	} catch (Exception e) {
+			//		e.printStackTrace();
+			//	}
+			//}
 		}
 	}
 }
