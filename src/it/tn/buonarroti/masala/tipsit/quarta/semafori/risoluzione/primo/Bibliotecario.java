f95a4b9ddbfe232b6a126e43ac0f00947d2c4900
Francesco Masala <mail@francescomasala.me>
2022-10-10 12:55:58 +0200

    Updates

--- src/it/tn/buonarroti/masala/tipsit/quarta/semafori/risoluzione/primo/Bibliotecario.java
+++ src/it/tn/buonarroti/masala/tipsit/quarta/semafori/risoluzione/primo/Bibliotecario.java
@@ -1,7 +1,12 @@
 package it.tn.buonarroti.masala.tipsit.quarta.semafori.risoluzione.primo;
 
+
+import java.util.concurrent.Semaphore;
+
 public class Bibliotecario {
 	protected int waitTime;
+
+	Semaphore sem = new Semaphore(1);
 
 	public Bibliotecario(int waitTime) {
 		this.waitTime = waitTime;
@@ -15,5 +20,18 @@ public class Bibliotecario {
 			System.out.println("Thread Interrotto");
 		}
 		System.out.println(tname + " ha trovato un libro");
+	}
+
+	public void ricercaSem(String tName) {
+		try {
+			System.out.println(tName + "Sta cercando un libro");
+			sem.acquire();
+			Thread.sleep(waitTime);
+			System.out.println(tName + "Ha trovato il libro");
+		} catch (InterruptedException e) {
+			System.out.println(tName + "Thread Interrotto");
+
+		}
+
 	}
 }
